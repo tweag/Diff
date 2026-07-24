9e087307ccd2ff865731fc9c46ee966da9bc98a2
Facundo Domínguez <facundo.dominguez@tweag.io>
2026-07-24 11:03:55 +0000

    Add executable for testing allocations

---
+++ test/exe.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+import Control.DeepSeq
+import GHC.Generics
+import System.Random
+
+import Data.Algorithm.Diff
+
+deriving instance Generic (Diff a)
+
+instance NFData a => NFData (Diff a)
+
+
+main :: IO ()
+main = doBenchMarks 37
+
+doBenchMarks :: Int -> IO ()
+doBenchMarks seed =
+  let rbools = randoms (mkStdGen seed) :: [Bool]
+      (s1000_1, rbools1) = splitAt 10000 rbools
+      (s1000_2, rbools2) = splitAt 10000 rbools1
+      s500_2 = take 500 s1000_2
+      res = getDiff s1000_1 s1000_2
+  in s1000_1 `deepseq` s1000_2 `deepseq` s500_2 `deepseq` res  `deepseq` pure ()
